68ca592dc4531c57653889833c16cc7a6e67008a
yoshihiro-darkness <darkness.okkake@gmail.com>
2018-03-11 05:45:37 +0000

    user registration for tasklists

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,6 +28,10 @@ class AuthController extends Controller
      *
      * @return void
      */
+
+	//add
+	protected $redirectTo = '/';
+
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'getLogout']);
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', 'TasklistsController@index');
+//Route::get('/', 'TasklistsController@index');
+Route::get('/', function() {
+	return view('welcome');
+});
 
 Route::resource('tasklists', 'TasklistsController');
+
+//ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
